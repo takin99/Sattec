e689df0bc8dad5f959cf41545e6bcab128ae390d
Kosar Ghasempour <117743366+takin99@users.noreply.github.com>
2022-11-23 17:01:56 +0330

    update IApplicationDbContext

--- src/Services/Identity/Identity.Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Services/Identity/Identity.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -6,5 +6,6 @@ namespace sattec.Identity.Application.Common.Interfaces;
 public interface IApplicationDbContext
 {
  //   DbSet<User> User { get; }
+     DbSet<BankAccount> BankAccounts { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
